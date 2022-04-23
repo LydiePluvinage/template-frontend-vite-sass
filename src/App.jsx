14f4b5593f84eff9b5a1baf9365fccd97eb5d362
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-04-23 16:47:29 +0200

    test vercel

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className="app">
       <p className="app__text">
         Ce template est bien configuré pour vos applications front-end avec
-        Vite, Sass et Parcel
+        Vite, Sass et Vercel
       </p>
     </div>
   );
