a832f22a5f7e8b72dc17b5579b0d9b83eb6a4974
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-04-23 16:31:06 +0200

    Vercel configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.scss";
+
 function App() {
   return (
     <div className="app">
       <p className="app__text">
-        Ce template est bien configuré pour vos applications front-end avec Vite
-        et Sass
+        Ce template est bien configuré pour vos applications front-end avec
+        Vite, Sass et Parcel
       </p>
     </div>
   );
